4b5fadd88209216a0aa60a685f4716831bec851e
iDiego5614 <diego5614yt@gmail.com>
2022-05-04 20:54:27 -0500

    feat(frontend): 🏘️Pages #1

---
+++ frontend/pages/index.js
@@ -0,0 +1,11 @@
+const Home = () => 
+{
+  return(
+    <div>
+      Discord Home
+    </div> 
+  )
+}
+
+
+export default Home
---
+++ frontend/pages/register.jsx
@@ -0,0 +1,18 @@
+import styles from "../styles/Auth.module.css"
+
+const register = () => 
+{
+  return(
+    <div className={styles.auth}>
+      <main className={styles.auth__card}>
+        <h1>Register on Discord!</h1>
+
+        <form>
+          
+        </form>
+      </main>
+    </div>
+  )
+}
+
+export default register
